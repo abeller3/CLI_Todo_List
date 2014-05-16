892e5a5d1f3b9816f56280035a12c3a0180b9070
abeller3 <beller.amanda@yahoo.com>
2014-05-16 15:02:55 -0500

    adjusted functions

--- todo.php
+++ todo.php
@@ -1,51 +1,69 @@
-
-
 <?php
 
 // Create array to hold list of todo items
 $items = array();
-array_unshift($items, "");
-unset($items[0]);
+
+
+// $var += 5;
+// $var = $var + 5;
+
+// $var = $var . 'some value';
+// $var .= 'some value';
+
+// List array items formatted for CLI
+function list_items($list)
+{
+    $result = '';
+
+    foreach($list as $key => $value)
+    {
+        $add = $key + 1;
+        $result .= '[' . $add . '] ' . $value . PHP_EOL;
+    }
+
+    return $result;
+}
+
+echo list_items($items);
+
+
+
+// Get STDIN, strip whitespace and newlines, 
+// and convert to uppercase if $upper is true
+function get_input($upper = false) 
+{
+    $result = trim(fgets(STDIN));
+    return $upper ? strtoupper($result) : $result;
+}
 
 // The loop!
 do {
-    //Iterate through list items
-    foreach ($items as $key => $item) {
-        // Display each item and a newline
-        echo "[{$key}] {$item}\n";
-    }
-
-    // for($i = 1; $i <count($items); $i++) {
-    // 	$key = $items[$i];
-    // 	echo . PHP_EOL;
-
-    // }
+    // Echo the list produced by the function
+    echo list_items($items);
 
     // Show the menu options
     echo '(N)ew item, (R)emove item, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-
-    $input = trim(fgets(STDIN));
-    // $input = ucfirst($input);
-
+    $input = get_input(TRUE);
 
     // Check for actionable input
     if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
-        $items[] = trim(fgets(STDIN));
+        $items[] = get_input();
     } elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
-        $key = trim(fgets(STDIN));
+        $key = get_input();
+        $key--;
         // Remove from array
         unset($items[$key]);
     }
-// Exit when input is (Q)uitn
+// Exit when input is (Q)uit
 } while ($input != 'Q');
 
 // Say Goodbye!
