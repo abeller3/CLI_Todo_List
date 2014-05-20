70d0d6cbcaecf9ab7564d7f003e63bf883f28ba3
abeller3 <beller.amanda@yahoo.com>
2014-05-20 14:35:36 -0500

    added sort to function

--- list_with_commas.php
+++ list_with_commas.php
@@ -32,6 +32,7 @@
 // Converts array into list n1, n2, ..., and n3
 function humanized_list($string) {
   $array = explode(', ',$string);
+  sort($array);
   $last_item = array_pop($array);
   return implode(', ', $array) . ",and $last_item";
 }
@@ -44,5 +45,7 @@ $famous_fake_physicists = humanized_list($physicists_string);
 
 // Output sentence
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}." . PHP_EOL;
+
+
 
 ?>
